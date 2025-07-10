ce828509fe9b3f37dae9ef001cc6215b84970204
storosso <storossoteam@gmail.com>
2025-07-10 07:42:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,23 @@ const url = require('url');
 
 const server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true);
-  
+
+  // CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    res.writeHead(204);
+    return res.end();
+  }
+
   if (parsedUrl.pathname === '/healthz') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('OK');
   } else if (parsedUrl.pathname === '/collect') {
-    res.writeHead(204); // No Content response
+    res.writeHead(204);
     res.end();
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
